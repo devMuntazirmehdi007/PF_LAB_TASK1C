3f123cf528eb6536c1bc85dcb5705b624653a30c
Muntazir Mehdi <muntaazirmehdi2@gmail.com>
2026-09-16 07:53:45 -0700

    Update square.c

---
+++ POST LAB PROJECT/square.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    int square, cube;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    square = number * number;
+    cube = number * number * number;
+
+    printf("Square = %d\n", square);
+    printf("Cube = %d\n", cube);
+
+    return 0;
+}
